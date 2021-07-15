60c5026528a80ecc7f151f9116d390570739d60b
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-15 10:31:03 -0700

    Update coin.rb with class Coins, and add quarter to function; second test pass
    co-authored-by: Andrew Giang <giang184@gmail.com>
    Co-authored-by: Arthur Lee <meleearthur@gmail.com>
    Co-authored-by: Araceli Valdovinos <valdovinosaraceli50@gmail.com>

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,5 +1,39 @@
-def coin(value)
-  if value%1 === 0
-    amount = "pennies: #{value}"
+class Coins
+  attr_accessor(:penny_counter, :nickel_counter, :dime_counter, :quarter_counter)
+
+  def initialize()
+    @penny_counter = 0
+    @nickel_counter = 0
+    @dime_counter = 0
+    @quarter_counter = 0
   end
+
+  def coin(value)
+    until (value == 0) do
+      if value >= 25
+        value -= 25
+        @quarter_counter += 1
+      elsif value < 5
+        value -= 1
+        @penny_counter += 1
+      end
+    end
+    coin_bank = "
+    Pennies: #{@penny_counter}
+    Quarter: #{@quarter_counter}
+    "
+  end 
 end
+
+
+
+
+
+
+#Notes 
+#Value is in cents
+#value > .99
+#store it in var
+#counter of coins
+#have a until loop that it has to reach 0 to stop it
+#The until loop subtracts 25, 10, 5, 1 based on if the number is greater than the number.
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -2,9 +2,21 @@ require('rspec')
 require('coin')
 require('pry')
 
-describe('#coin') do
+describe('Coins') do
   it("return a penny for one cent") do
-    expect(coin(1)).to(eq("pennies: 1"))
+    bank = Coins.new()
+    expect(bank.coin(1)).to(eq("
+    Pennies: 1
+    Quarter: 0
+    "))
+  end
+
+  it("return a quarter for values < 25") do
+    bank = Coins.new()
+    expect(bank.coin(26)).to(eq("
+    Pennies: 1
+    Quarter: 1
+    "))
   end
 end
 
